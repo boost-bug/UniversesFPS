6d4dc3a2888917b944eb623567deaf7e997e793f
Fishy Pug Bruh <fpbmail@mail.com>
2021-12-21 22:46:50 +0000

    added qodot plugin

--- scripts/startGameBtn.gd
+++ scripts/startGameBtn.gd
@@ -1,6 +1,11 @@
 extends Button
 
 
-
 func _on_Button_button_down():
-	get_tree().change_scene("res://Main.tscn")
+	get_tree().change_scene("res://assets/maps/CrossGray.tscn")
+
+func _on_Button_mouse_entered():
+	modulate.a = 0.4
+
+func _on_Button_mouse_exited():
+	modulate.a = 1
